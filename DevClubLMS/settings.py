a3e137db28a431454452f72650cf6623a5109c0c
Mukund Aggarwal <mukundagg24@gmail.com>
2022-07-29 19:53:15 +0530

    Procfile changes

--- DevClubLMS/settings.py
+++ DevClubLMS/settings.py
@@ -124,14 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+STATIC_URL = 'static/'
+STATIC_ROOT = "static/"
+if DEBUG:
+    STATIC_ROOT = ''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
 MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIAFILES_DIRS = (
     os.path.join(BASE_DIR, 'media/'),
 )
