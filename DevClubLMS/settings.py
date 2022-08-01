ecca67c4d10c91a8572cde9a3df3b69d8278efd8
Mukund Aggarwal <mukundagg24@gmail.com>
2022-08-01 21:33:49 +0530

    Setting security update

--- DevClubLMS/settings.py
+++ DevClubLMS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,6 +87,10 @@ WSGI_APPLICATION = 'DevClubLMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 DATABASES = {
     'default': {
